74bd288021e55db6bb0717f7c73a52c37f84c62b
Muhammed <mmkader85@gmail.com>
2016-11-15 07:55:47 +0800

    Youtube initial search and click to load videos

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,25 @@
 /**
  * Created by muhammed on 13/11/16.
  */
+
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    );
+  });
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+};
+
+export default VideoList;
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,28 @@
 /**
  * Created by muhammed on 13/11/16.
  */
+
+import React from 'react';
+
+// ES6 - Extract video property from object
+const VideoListItem = ({video, onVideoSelect}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
